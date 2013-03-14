5be99a6545811dffdb892f80e2b0e5c7d1c13dea
Bodo Tasche <bodo@wannawork.de>
2013-03-14 21:23:17 +0100

    added font module, fixes #3

---
+++ lib/exogenesis/fonts.rb
@@ -0,0 +1,46 @@
+require 'exogenesis/abstract_package_manager'
+
+# Installs and removes OhMyZSH
+class Fonts < AbstractPackageManager
+
+  # This copies fonts from the dotfiles repo
+  # to the directory where Mac OS expects the
+  # Font. You can change the directory within
+  # the repo if you want.
+  def initialize(basepath = "fonts")
+    @basepath = basepath
+  end
+
+  def install
+    collect_fonts do |file|
+      install_font(file)
+    end
+  end
+
+  def teardown
+    collect_fonts do |file|
+      uninstall_font(File.basename(file))
+    end
+  end
+
+  private
+
+  def collect_fonts
+    Dir.glob(File.join(@basepath, "**/*.ttf")).each do |file|
+      yield file
+    end
+    Dir.glob(File.join(@basepath, "**/*.otf")).each do |file|
+      yield file
+    end
+  end
+
+  def install_font(file)
+    puts "Installing #{File.basename file}"
+    FileUtils.cp file, File.join(ENV['HOME'], "Library/Fonts", File.basename(file))
+  end
+
+  def uninstall_font(file)
+   puts "Uninstalling #{file}"
+   FileUtils.rm File.join(ENV['HOME'], "Library/Fonts", file)
+  end
+end
