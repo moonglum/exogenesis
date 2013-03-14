a3949a60c9de71dc8ef8bf7be27fb28f7e8e333b
moonglum <moonglum@moonbeamlabs.com>
2013-03-14 20:03:22 +0100

    Let everyone use their own OhMyZSH Fork. Fixes #2

--- lib/exogenesis/oh-my-zsh.rb
+++ lib/exogenesis/oh-my-zsh.rb
@@ -2,6 +2,12 @@ require 'exogenesis/abstract_package_manager'
 
 # Installs and removes OhMyZSH
 class OhMyZSH < AbstractPackageManager
+  # If you want to use your own fork, just give your username
+  # as an argument. The default is of course 'robbyrussell'.
+  def initialize(username = "robbyrussell")
+    @repo = "git://github.com/#{username}/oh-my-zsh.git"
+  end
+
   def install
     print "Cloning Oh-my-zsh..."
     target = File.join Dir.home, ".oh-my-zsh"
@@ -9,7 +15,7 @@ class OhMyZSH < AbstractPackageManager
     if File.exists? target
       puts "Oh-my-zsh already exists"
     else
-      `git clone git://github.com/moonglum/oh-my-zsh.git #{target}`
+      `git clone #{@repo} #{target}`
       puts "Cloned!"
     end
   end
