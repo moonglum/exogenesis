909e1773c7474f8a056772bd411163b622ab12d6
moonglum <moonglum@moonbeamlabs.com>
2013-03-17 19:26:41 +0100

    Vundle is now using Executor

--- lib/exogenesis/vundle.rb
+++ lib/exogenesis/vundle.rb
@@ -1,53 +1,52 @@
 require 'exogenesis/support/abstract_package_manager'
+require 'exogenesis/support/executor'
 
 # Manages the Vim Package Manager Vundle
 class Vundle < AbstractPackageManager
-  # The dependencies are read from your Vim files
-  # It creates a `~/.vim` folder and clones Vundle.
-  # Then it runs BundleInstall in Vim.
-  def install
-    create_vim_folder
-    clone
-    bundle_install
+  def initialize
+    @executor = Executor.instance
+    @vundle_repo = "git://github.com/gmarik/vundle.git"
   end
 
-  def teardown
-    print "Removing Vundle..."
-    target = File.join Dir.home, ".vim"
+  # The dependencies are read from your Vim files
+  # It creates a `~/.vim` folder and clones Vundle.
+  def setup
+    @executor.start_section "Vundle"
+    Dir.mkdir(vim_folder) unless File.exists? vim_folder
 
-    if File.exists? target
-      `rm -r #{target}`
-      puts "Removed!"
-    else
-      puts "Did not exist"
+    @executor.execute "Cloning Vundle", "git clone #{@vundle_repo} #{vundle_folder}" do |output, error_output|
+      raise TaskSkipped.new("Already exists") if error_output.include? "already exists"
     end
   end
 
+  # Runs BundleInstall in Vim.
+  def install
+    @executor.start_section "Vundle"
+    @executor.execute_interactive "Install", "vim +BundleInstall\! +qall"
+    @executor.execute_interactive "Clean", "vim +BundleClean\! +qall"
+  end
+
+  # Removes the ~/.vim folder
+  def teardown
+    @executor.start_section "Vundle"
+    @executor.execute "Removing Vim Folder", "rm -r #{vim_folder}" do |output|
+      raise TaskSkipped.new("Folder not found") if output.include "No such file or directory"
+    end
+  end
+
+  # Updates all installed vundles
   def update
-    puts "Updating Vim Bundles"
-    system "vim +BundleUpdate +qall"
+    @executor.start_section "Vundle"
+    @executor.execute_interactive "Updating Vim Bundles", "vim +BundleUpdate +qall"
   end
 
   private
 
-  def create_vim_folder
-    target = File.join Dir.home, ".vim"
-    Dir.mkdir target unless File.exists? target
+  def vim_folder
+    File.join Dir.home, ".vim"
   end
 
-  def clone
-    target = File.join Dir.home, ".vim", "bundle", "vundle"
-
-    if File.exists? target
-      puts "Vundle already exists"
-    else
-      `git clone git://github.com/gmarik/vundle.git #{target}`
-      puts "Cloned!"
-    end
-  end
-
-  def bundle_install
-    system "vim +BundleInstall\! +qall"
-    system "vim +BundleClean\! +qall"
+  def vundle_folder
+    File.join Dir.home, ".vim", "bundle", "vundle"
   end
 end
