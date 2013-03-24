82e6f2eeba86c450d14696791814b720a2c60bb0
moonglum <moonglum@moonbeamlabs.com>
2013-03-24 23:35:12 +0100

    1.8.7 specific fix

--- spec/shared/package_manager_spec.rb
+++ spec/shared/package_manager_spec.rb
@@ -6,7 +6,8 @@ shared_examples "a package manager" do |params|
   params[:for].each do |method|
     it "should start the section '#{params[:with_section_name]}' in #{method}" do
       executor.should_receive(:start_section).with(params[:with_section_name].to_s)
-      subject.public_send(method)
+      # Using send instead of public_send, because we want to run on 1.8.7
+      subject.send(method)
     end
   end
 end
