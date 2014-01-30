2c7740a6ba073db32a95a2df7cdaa0ee2494f6d3
Bodo Tasche <bodo@wannawork.de>
2014-01-30 20:40:47 +0100

    added npm support, closes #17

---
+++ lib/exogenesis/passengers/npm.rb
@@ -0,0 +1,26 @@
+require 'exogenesis/support/passenger'
+
+# Install NPM and NPM packages
+# REQUIRES: Homebrew (so put it after your homebrew task)
+class Npm < Passenger
+  register_as :npm
+  needs :npms
+
+  def setup
+    execute "Install npm", "brew install npm" do |output|
+      raise TaskSkipped.new("Already installed") if output.include? "already installed"
+    end
+  end
+
+  def install
+    npms.each do |package|
+      execute "Install #{package}", "npm install -g #{package}"
+    end
+  end
+
+  def update
+    npms.each do |package|
+      execute "Update #{package}", "npm update -g #{package}" 
+    end
+  end
+end
