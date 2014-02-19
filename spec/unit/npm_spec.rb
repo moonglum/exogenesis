0f53ece69b6746466d4e7a89cb608b99dbf389a8
moonglum <moonglum@moonbeamlabs.com>
2014-02-19 21:42:16 +0100

    Adjusted NPM passenger to #23

--- spec/unit/npm_spec.rb
+++ spec/unit/npm_spec.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 require "spec_helper"
 require "exogenesis/passengers/npm"
 
@@ -6,33 +7,52 @@ describe Npm do
   before { allow(config).to receive(:npms).and_return(npms) }
 
   let(:executor) { executor_double }
-  let(:npms) { ["buster"] }
+  let(:npms) { ['bower', 'buster'] }
+
+  let(:raw_installed) { "/usr/local/share/npm/lib\n├── bower@1.2.8\n├── docco@0.6.3" }
 
   subject { Npm.new(config, executor) }
 
-  describe :setup do
-    it "should install npm via homebrew and then link it with overwrite" do
-      executor.should_receive(:execute).ordered.with("Install npm", "brew install npm")
-      subject.setup
-    end
-
-    it "should skip if npm is already installed" do
-      executor.stub(:execute).with("Install npm", "brew install npm").and_yield "already installed"
-      expect { subject.setup }.to raise_exception(TaskSkipped, "Already installed")
-    end
-  end
-
-  describe :install do
-    it "should install the npms provided when initialized" do
-      executor.should_receive(:execute).with("Install buster", "npm install -g buster")
-      subject.install
-    end
-  end
-
   describe :up do
-    it "should update the npms provided when initialized" do
-      executor.should_receive(:execute).with("Update buster", "npm update -g buster")
-      subject.up
+    before { allow(executor).to receive(:silent_execute).with('npm ls -g --depth=0').and_return(raw_installed) }
+
+    describe 'install Node if necessary' do
+      context 'when npm command was not found' do
+        before { allow(executor).to receive(:command_exists?).with('npm').and_return(false) }
+
+        it 'should install node via homebrew' do
+          expect(executor).to receive(:execute).with('Install Node', 'brew install node')
+          subject.up
+        end
+      end
+
+      context 'when npm command was found' do
+        before { allow(executor).to receive(:command_exists?).with('npm').and_return(true) }
+
+        it 'should not install node via homebrew' do
+          expect(executor).to_not receive(:execute).with('Install Node', 'brew install node')
+          subject.up
+        end
+
+        it 'should indicate that it skipped installing Node' do
+          expect(executor).to receive(:skip_task).with('Install Node')
+          subject.up
+        end
+      end
+    end
+
+    describe 'install or update packages' do
+      before { allow(executor).to receive(:command_exists?).with('npm').and_return(true) }
+
+      it 'should install missing packages' do
+        executor.should_receive(:execute).with('Install buster', 'npm install -g buster')
+        subject.up
+      end
+
+      it 'should update installed packages' do
+        executor.should_receive(:execute).with('Update bower', 'npm update -g bower')
+        subject.up
+      end
     end
   end
 end
